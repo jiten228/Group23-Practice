bf8a0d684af891132380d07ef7aee0f5fc5b44ca
jiten228 <jiten228@yahoo.com>
2020-11-12 19:31:20 -0600

    How are you?

--- src/Murat/TC002.java
+++ src/Murat/TC002.java
@@ -3,7 +3,11 @@ package Murat;
 public class TC002 {
 
     public static void main(String[] args) {
+
         System.out.println("Hi everbody");
+        System.out.println( "Hi Murat, How are you?" );
+        System.out.println( "Good Night See you Tomorrow" );
     }
+
 }
 //more work here
